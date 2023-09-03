368f40602074a31d7cd555dd90e327486a706f77
samantrs <https://samantrs@git.rabobank.nl>
2023-09-03 14:12:51 +0200

    first commit

---
+++ kafka-cloud-stream/src/main/java/com/spring/event/streaming/kafkacloudstream/KafkaCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package com.spring.event.streaming.kafkacloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCloudStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCloudStreamApplication.class, args);
+	}
+
+}
---
+++ kafka-cloud-stream/src/test/java/com/spring/event/streaming/kafkacloudstream/KafkaCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.event.streaming.kafkacloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCloudStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
