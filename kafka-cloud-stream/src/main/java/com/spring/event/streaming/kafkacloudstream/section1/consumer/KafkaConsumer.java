ed36dbeea07f3402db6f8d2f29ccb09a7feda745
samantrs <https://samantrs@git.rabobank.nl>
2023-09-03 20:21:49 +0200

    spring cloud consumer receiver options

--- kafka-cloud-stream/src/main/java/com/spring/event/streaming/kafkacloudstream/section1/consumer/KafkaConsumer.java
+++ kafka-cloud-stream/src/main/java/com/spring/event/streaming/kafkacloudstream/section1/consumer/KafkaConsumer.java
@@ -1,10 +1,13 @@
 package com.spring.event.streaming.kafkacloudstream.section1.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.cloud.stream.binder.reactorkafka.ReceiverOptionsCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.kafka.receiver.ReceiverOptions;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -27,6 +30,14 @@ public class KafkaConsumer {
         return stringFlux -> stringFlux
                 .doOnNext(message -> log.info("message received by function: {}", message))
                 .then();
+    }
+
+    @Bean
+    public ReceiverOptionsCustomizer<String, ReceiverOptions> receiverOptionsCustomizer() {
+        return (bindingName, receiverOptions) -> {
+            log.info("Kafka binding name: {}", bindingName);
+            return receiverOptions.consumerProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "f8c3de3d");
+        };
     }
 
 }
