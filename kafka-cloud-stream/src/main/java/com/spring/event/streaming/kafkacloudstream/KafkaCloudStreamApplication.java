486bde5c3e88e5ab1b84cda2514e656e3c609495
samantrs <https://samantrs@git.rabobank.nl>
2023-09-03 19:21:36 +0200

    spring cloud consumer as function and created profiles

--- kafka-cloud-stream/src/main/java/com/spring/event/streaming/kafkacloudstream/KafkaCloudStreamApplication.java
+++ kafka-cloud-stream/src/main/java/com/spring/event/streaming/kafkacloudstream/KafkaCloudStreamApplication.java
@@ -3,7 +3,7 @@ package com.spring.event.streaming.kafkacloudstream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.spring.event.streaming.kafkacloudstream.${section}")
 public class KafkaCloudStreamApplication {
 
 	public static void main(String[] args) {
--- kafka-cloud-stream/src/main/java/com/spring/event/streaming/kafkacloudstream/section1/consumer/KafkaConsumer.java
+++ kafka-cloud-stream/src/main/java/com/spring/event/streaming/kafkacloudstream/section1/consumer/KafkaConsumer.java
@@ -1,21 +1,32 @@
-package com.spring.event.streaming.kafkacloudstream.consumer;
+package com.spring.event.streaming.kafkacloudstream.section1.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Slf4j
 @Configuration
 public class KafkaConsumer {
 
+    // an example of a simple spring cloud stream kafka consumer
     @Bean
     public Consumer<Flux<String>> stringConsumer() {
         return stringFlux -> stringFlux
                 .doOnNext(message -> log.info("message received by consumer: {}", message))
                 .subscribe();
+    }
+
+    // spring cloud kafka consumer as a function
+    @Bean
+    public Function<Flux<String>, Mono<Void>> stringFunction() {
+        return stringFlux -> stringFlux
+                .doOnNext(message -> log.info("message received by function: {}", message))
+                .then();
     }
 
 }
